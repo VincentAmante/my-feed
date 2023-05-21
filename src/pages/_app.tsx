b7b6c4b3d32b0676c9a3f6cd4b0df0a1e1c29b40
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-21 23:49:08 +0400

    Added sign in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { type AppType } from "next/app";
 // page/_app.tsx
 // import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/clerk-react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return     <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />
+    </ClerkProvider>;
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -3,6 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return <>
   <div>Hello ch-ch-ch</div>
-  <SignIn />
+  {/* <SignIn /> */}
   </>;
 }
