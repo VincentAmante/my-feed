8734b0dc7a7e6dc66a984e2afa8db4d3041ace66
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-22 00:00:56 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { UserButton } from "@clerk/clerk-react";
 
 import { api } from "~/utils/api";
 
@@ -15,7 +16,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
+      <UserButton />
       </main>
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
