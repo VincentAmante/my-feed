d61950a076c688ed4a3ca7e2855c8708ea210024
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-21 23:13:32 +0400

    Added ClerkAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { type AppType } from "next/app";
-
+// page/_app.tsx
+import { ClerkProvider } from "@clerk/nextjs";
+// import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <ClerkProvider {...pageProps}>
+            <Component {...pageProps} />
+          </ClerkProvider>;
 };
 
 export default api.withTRPC(MyApp);
