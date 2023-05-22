abb665f0e1e70e250d2145c29ec37241f886b34c
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-22 11:49:07 +0400

    Reverts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <UserButton />
+      {/* <UserButton /> */}
       </main>
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
