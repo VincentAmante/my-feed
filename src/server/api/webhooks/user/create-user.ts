3958e420e6b56e2da5318b05ae1a502a9f56d931
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-26 23:57:43 +0400

    Update create-user.ts

--- src/server/api/webhooks/user/create-user.ts
+++ src/server/api/webhooks/user/create-user.ts
@@ -1,52 +1,71 @@
+import type { IncomingHttpHeaders } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { WebhookRequiredHeaders } from "svix";
+import type { User } from "@clerk/nextjs/dist/api";
+import { Webhook } from "svix";
 import { prisma } from "~/server/db";
-import { z } from "zod";
 
-export const clerkWebhookSchema = z.object({
-  type: z.enum(["user.created", "user.deleted"]),
-  data: z.object({
-    id: z.string(),
-  }),
-});
+type UnwantedKeys = "emailAddresses" | "firstName" | "lastName" | "primaryEmailAddressId" | "primaryPhoneNumberId" | "phoneNumbers";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log(req);
-
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-    return;
-  } else {
-    const valid = clerkWebhookSchema.safeParse(req.body);
-    if (!valid.success) {
-      res.status(400).json({ message: "Invalid request" });
-      return;
-    } else {
-      const parsed = valid.data;
-
-      switch (parsed.type) {
-        case "user.created": {
-          await prisma.user.create({
-            data: {
-              authId: parsed.data.id,
-            },
-          });
-          console.log("user created", parsed.data.id);
-          break;
-        }
-        case "user.deleted": {
-          await prisma.user.delete({
-            where: {
-              authId: parsed.data.id,
-            },
-          });
-          console.log("user deleted", parsed.data.id);
-          break;
-        }
-      }
-      res.status(200).end();
-    }
+interface UserInterface extends Omit<User , UnwantedKeys>{
+    email_addresses: {
+      email_address: string;
+      id: string;
+    }[];
+    primary_email_address_id: string;
+    first_name: string;
+    last_name: string;
+    primary_phone_number_id: string;
+    phone_numbers: {
+        phone_number: string;
+        id: string;
+    }[];
   }
-}
+  
+  const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+  
+  export default async function handler(
+    req: NextApiRequestWithSvixRequiredHeaders,
+    res: NextApiResponse
+  ) {
+    const payload = JSON.stringify(req.body);
+    const headers = req.headers;
+    const wh = new Webhook(webhookSecret);
+    let evt: Event | null = null;
+    try {
+      evt = wh.verify(payload, headers) as Event;
+    } catch (_) {
+      return res.status(400).json({});
+    }
+    const { id } = evt.data;
+    // Handle the webhook
+    const eventType: EventType = evt.type;
+    if (eventType === "user.created" || eventType === "user.updated") {
+        const { first_name,last_name} = evt.data;
+
+        await prisma.user.upsert({
+          where: { authId: id },
+          update: {
+            name: ``,
+          },
+          create: {
+            authId: id,
+            name: `${first_name || ""} ${last_name || ""}`,
+          },
+        });
+      }
+      console.log(`User ${id} was ${eventType}`);
+      res.status(201).json({});
+    }
+  
+  type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+    headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+  };
+  
+  type Event = {
+    data: UserInterface ;
+    object: "event";
+    type: EventType;
+  };
+  
+  type EventType = "user.created" | "user.updated" | "*";
