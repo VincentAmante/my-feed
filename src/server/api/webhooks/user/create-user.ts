239d1aaaeb4cada50959678bf2d76b8572168df9
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-27 08:27:34 +0400

    Update create-user.ts

--- src/server/api/webhooks/user/create-user.ts
+++ src/server/api/webhooks/user/create-user.ts
@@ -2,3 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ message: "it works" });
 }
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const PATCH = handler;
+export const DELETE = handler;
