e214de220bfe03cff9d2369e2bfaa4cd60566b11
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-06-02 17:53:28 +0400

    Added handling of posts without avatars

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos', 'res.cloudinary.com', 'images.clerk.dev'],
+    domains: ['picsum.photos', 'res.cloudinary.com', 'images.clerk.dev', 'www.gravatar.com'],
   },
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
