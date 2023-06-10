28be530e56a5e497c3441addcc221c7dc9aa8edf
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-06-11 00:55:58 +0400

    Switched sign-up modal

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -8,10 +8,14 @@ import LogoAnimated from "~/components/Logo/Animated";
 // };
 
 const SignUpPage = () => (
-  <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-    <section className="flex flex-col items-center gap-2 border-b-2 border-b-primary px-4 py-4 text-center">
-      <LogoAnimated />
-      <h1 className="text-6xl font-semibold">Welcome to Kiurate</h1>
+  <main className="flex min-h-screen flex-col items-center justify-center gap-4  lg:grid lg:grid-cols-2 lg:px-28">
+    <section className="flex flex-col items-center gap-6 border-b-2 border-primary px-12 py-4 text-center lg:border-b-0 lg:border-r-2">
+      <div className="max-w-xs">
+        <LogoAnimated />
+      </div>
+      <h1 className="text-6xl font-semibold uppercase text-secondary">
+        Kiurate
+      </h1>
       <h2 className="max-w-sm text-2xl">
         Your space, your content, your experience.
       </h2>
