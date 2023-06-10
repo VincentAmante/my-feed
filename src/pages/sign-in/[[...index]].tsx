dfc38da46ce0a34dc0db2612b54c85440ee0d8a6
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-06-11 01:00:04 +0400

    Update [[...index]].tsx

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,5 +1,6 @@
 import { SignIn, SignedIn } from "@clerk/nextjs";
 import { useRouter } from "next/router";
+import LogoAnimated from "~/components/Logo/Animated";
 
 const Redirect = () => {
   // Redirects users to a handler page
@@ -11,13 +12,21 @@ const Redirect = () => {
 export default function Page() {
   return (
     <main className="grid min-h-screen items-center justify-center lg:grid-cols-2">
-      <section className="flex flex-col items-center gap-2 px-4 text-center">
-        <h1 className="max-w-md text-6xl">Welcome back to Kiurate!</h1>
-        <h2 className="max-w-sm text-2xl">We missed you ^-^</h2>
+      <section className="flex flex-col items-center gap-6  px-12 py-4 text-center">
+        <div className="max-w-xs">
+          <LogoAnimated />
+        </div>
+        <h1 className="text-6xl font-semibold uppercase text-secondary">
+          Kiurate
+        </h1>
+        <h2 className="flex max-w-xs flex-col text-2xl">
+          <span> Welcome back! </span>
+          <span>We missed you!</span>
+        </h2>
       </section>
 
       {/* Sign in from Clerk */}
-      <section>
+      <section className="flex flex-col items-center justify-center px-4 lg:items-start lg:justify-start lg:px-0">
         <SignIn />
         <SignedIn>
           <Redirect />
